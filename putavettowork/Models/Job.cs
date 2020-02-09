0ee3557e8c8b8ee071684383f48556649200bb42
JRA-sencio <56657391+JRA-sencio@users.noreply.github.com>
2020-02-09 15:59:13 -0800

    Added View Model

---
+++ putavettowork/Models/Job.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace putavettowork.Models
+{
+    public class Job
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public double Salary { get; set; }
+        public string Description { get; set; }
+        public string Location { get; set; }
+        public DateTime Created { get; set; }
+        public byte[] Picture { get; set; }
+        public DateTime Established { get; set; }
+
+        public int JobSearchId { get; set; }
+
+        public JobSearch JobSearch { get; set; }
+    }
+}
+ //public enum DifficultyType {Easy, Moderate, Difficult, Expert }
+        //public DifficultyType Difficulty { get; set; }
