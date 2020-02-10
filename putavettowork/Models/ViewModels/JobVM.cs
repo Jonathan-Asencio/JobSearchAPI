f465936ba452bbdebf2c1d9bff0f6fbfc2d85beb
JRA-sencio <56657391+JRA-sencio@users.noreply.github.com>
2020-02-09 16:12:25 -0800

    Changed Class name

---
+++ putavettowork/Models/ViewModels/JobVM.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace putavettowork.Models.ViewModels
+{
+    //Jobs View Model
+    public class JobVM
+    {
+        public IEnumerable<SelectListItem> JobSearchList { get; set; }
+        public Job Job { get; set; }
+
+    }
+}
