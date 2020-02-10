a081286d9af8136f2c0d3f490d9c149e10a00a50
JRA-sencio <56657391+JRA-sencio@users.noreply.github.com>
2020-02-09 16:14:54 -0800

    Corrected Parameters

--- putavettowork/wwwroot/js/JobSearch.js
+++ putavettowork/wwwroot/js/JobSearch.js
@@ -7,22 +7,22 @@ $(document).ready(function () {
 function loadDataTable() {
     dataTable = $('#tblData').DataTable({
         "ajax": {
-            "url": "/JobSearch/GetAllJobs",
-            "type": "Get",
+            "url": "/jobSearch/GetAllJobSearch",
+            "type": "GET",
             "datatype":"json"
         },
         "columns": [
             { "data": "name", "width": "50%" },
-            { "data": "state", "width": "20%" },
+            { "data": "location", "width": "20%" },
             {
                 "data": "id",
                 "render": function (data) {
                     return `<div class= "text-center">
-                                <a href="/JobSearch/Upsert/${data}" class= 'btn btn success text-white'
+                                <a href="/jobSearch/Upsert/${data}" class= 'btn btn success text-white'
                                     style= 'cursor:pointer;'> <i class='far fa-edit'></i></a>
                                     &nbsp;
                                     &nbsp;
-                                <a onclick=Delete("/JobSearch/Delete/${data}") class='btn btn-danger text-white'
+                                <a onclick=Delete("/jobSearch/Delete/${data}") class='btn btn-danger text-white'
                                     style='cursor:pointer;'> <i class='far fa-trash-alt'></i></a>
                                 </div>
                             `;
